28e2052702a49d9944b74e12824fcd256190cbcd
danna2d3 <danna.andrearg@gmail.com>
2023-01-03 13:23:16 -0500

    Abby

---
+++ main.js
@@ -0,0 +1,172 @@
+const menuEmail = document.querySelector ('.navbar-email');
+const desktopMenu = document.querySelector ('.desktop-menu')
+const menuHamIcon = document.querySelector ('.menu');
+const menuCarritoIcon = document.querySelector ('.navbar-shopping-cart');
+const mobileMenu = document.querySelector ('.mobile-menu');
+const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
+
+
+menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
+
+function toogleDesktopMenu () {
+    console.log ('Click')
+    desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu () {
+    const isAsideClosed = aside.classList.contains ('inactive');  
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+        }
+
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarritoAside () {
+    const ismobileMenuClosed = mobileMenu.classList.contains ('inactive');
+    //const isAsideClosed = aside.classList.contains ('inactive');
+    if (!ismobileMenuClosed) {
+    mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle ('inactive');
+}
+
+const productList = [] ;
+productList.push({
+    name: 'The story begins',
+    price: 'Like OHH AHH',
+    image: 'https://cdnx.jumpseller.com/cakepopundertaking/image/26804875/resize/756/945?1662267021',
+});
+productList.push({
+    name: 'Page Two',
+    price: 'Cheer Up',
+    image: 'https://is2-ssl.mzstatic.com/image/thumb/Music124/v4/7d/46/66/7d4666f3-1dec-c3ae-e81c-1efef0ae5d6b/00602508870033_Cover.jpg/1200x1200bf-60.jpg',
+});
+productList.push({
+    name: 'TWICEcoaster:Lane 1',
+    price: 'TT',
+    image: 'https://0.soompi.io/wp-content/uploads/sites/8/2016/12/05225255/TWICE_-_TWICEcoaster_-_LANE_1.jpg?s=900x600&e=t',
+});
+productList.push({
+    name: 'TWICEcoaster:Lane 2',
+    price: 'knock Knock',
+    image: 'https://lh3.googleusercontent.com/D3wwDqmMGwm7-x-xMEgmuI3wusNGY0R_1lH9dvZkZj01yfphXaJsySNEmeSenKgef586iRlvyLxeMa8=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'SIGNAL',
+    price: 'SIGNAL',
+    image: 'https://lh3.googleusercontent.com/xVIoyjxjeYag4muBgyx9yHO3eYpv_jJQJhHN4YR5iBhMT92nF7sVBGVdGk0cp-e0wPMZNnTSsPhDjNtYfQ=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Twicetagram',
+    price: 'Likey',
+    image: 'https://lh3.googleusercontent.com/OjY8-5Aigvy4lAhq0hglQGjqdigy94ae4xSClmbrpsOcBgGlmnpoFoyQK-EsCEtZ6wnAZXYe05YQPfWX=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Merry & Happy',
+    price: 'Heart Shaker',
+    image: 'https://lh3.googleusercontent.com/bcPRRIinnDWCOc1qO7Tts9x1EJZDRWsm1EmX_75m00STX4hNzjrtdroCrXMGfPyQeOdugQ0Dh6ah9NsvRw=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'What is Love?',
+    price: 'What is Love?',
+    image: 'https://lh3.googleusercontent.com/HLnp2ibpDGdKGYLRJVQUkuqg68KxiaEsCPuS8aJH0PQInvXhUB33Tmx6LDqDfo3cIZZpAEnCjKxh2U-W6g=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Summer Nights',
+    price: 'Dance the Night Away',
+    image: 'https://lh3.googleusercontent.com/5VH6oY8UZgRU6uNDGPOcq3t4tG8t3GsEOA9J36nLBlMX145OmFCSDnuRRqwKXj1DDuNqKrSDREICKKWT=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'YES or YES',
+    price: 'YES or YES',
+    image: 'https://lh3.googleusercontent.com/phf4VrHYV87J-IKMdKi359rtxx6wmAFEl-32Tp40DdqY1qXIdC6EvU3WQkt2RfBl7Xc1hRr2CVqVvAbh=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'FANCY YOU',
+    price: 'FANCY',
+    image: 'https://lh3.googleusercontent.com/yStgp0sdn6Cg8EQQQU_Ph4LkvhSFLhOvzWPNgwG9Fkc4XnzsFKTWT9-Zjdr9Tl4OrVjYfmSX3BpbYVef=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Feel Special',
+    price: 'Feel Special',
+    image: 'https://lh3.googleusercontent.com/4RLLtuRpjnyst7Zw34xucjWVcHxD2gRt3NvbteiNB5sOlhQopC7AAw3NGinm1VcCtHTgFYqgVi1V4INA=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'MORE & MORE',
+    price: 'MORE & MORE',
+    image: 'https://lh3.googleusercontent.com/83LiL35aPU-dasNh_pT7wy_n3eL3mw6QR3VO9HGwcAm_GktxGgCFbIlM5nD0b7fUdtWSFyc1dxSZwQmQ=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Eyes wide open',
+    price: 'I CANT STOP ME',
+    image: 'https://lh3.googleusercontent.com/lhQnqdU_0lhyPbM5f3wWp_jEs2_46A_d1_baovMrWdAc2BS-QIaplee_fTTXJ--0YCvCZNTDXqfDeQ7d=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Taste of Love',
+    price: 'Alcohol-Free',
+    image: 'https://lh3.googleusercontent.com/lmbgcqGRdQLWqjyPgk7239RFd7bB2CRxf4JpS4ndFkZZzcVt1Ia6FHs6Kd4mbWwZQx6DLwHJVDRz3UrZ=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Perfect World',
+    price: 'Perfect World',
+    image: 'https://lh3.googleusercontent.com/Ne0puWfEjwritXCByBSJBrmNNCe6f2IY4Lml6d9nuAEuqm1MYvzaF1HXdx0bbrI7TvCBSTdBsRBBkXT0DQ=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Formula of Love: O+T=<3',
+    price: 'SCIENTIST',
+    image: 'https://lh3.googleusercontent.com/VPPfJl0x2QzxrpxPfLnv7zLEyB2RGJYEjPuUAvuD00RXm9tYK1U_ssjxQefKDMHLzmNRnuNLXGC1HRX5=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'Celebrate',
+    price: 'Celebrate',
+    image: 'https://lh3.googleusercontent.com/tsxlchay1KEc308qrpK9eL_Ppk3a8dkfdCRqZB5an3DgYogoXTf3kgNkD73yJLFis3me2os_4LuptoY=w544-h544-l90-rj',
+});
+productList.push({
+    name: 'BETWEEN 1&2',
+    price: 'Talk that Talk',
+    image: 'https://lh3.googleusercontent.com/AaOD6pswVtfEJE4RKsbeMAWe76F-Xgd263fHcqwQ1dxzt1vU71nauPpvrYDxrY0zDxX76xmbxutaQizxOQ=w544-h544-l90-rj',
+});
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+ //
+    const productImg = document.createElement ('img');
+    productImg.setAttribute('src', product.image);
+    
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoDiv = document.createElement('div');
+
+    const productPrice = document.createElement('p');
+    productPrice.innerText =  product.price;
+    const productName = document.createElement('p');
+    productName.innerText = product.name;
+
+    productInfoDiv.appendChild(productPrice);
+    productInfoDiv.appendChild(productName);
+
+    const productInfoFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src','./149125.png');
+
+    productInfoFigure.appendChild(productImgCart);
+
+    productInfo.appendChild(productInfoDiv);
+    productInfo.appendChild(productInfoFigure);
+
+    productCard.appendChild(productImg);
+    productCard.appendChild(productInfo);
+
+    cardsContainer.appendChild(productCard);
+
+
+
+
+}
